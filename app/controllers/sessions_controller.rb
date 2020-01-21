f9d9c92f03bb87741f96f654fc9f241f5a26b29b
”名前” <>
2020-01-21 05:47:38 +0000

    second commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to root_path, success: 'ログインに成功しました'
+    else
+      flash.now[:danger] = 'ログインに失敗しました'
+      render :new
+    end
+  end
+
+  private
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :name, presence: true,
                    length: {maximum: 15}
-  validates :email, {presence: true, uniqueness: true},
+  validates :email, presence: true,
                     format: {with: /[a-zA-Z0-9.+]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*/}
   validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i}
   
