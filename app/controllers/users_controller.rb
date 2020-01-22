325a40149d8ddf0338ac0de6eaec328c1b467694
”名前” <>
2020-01-22 04:29:22 +0000

    view調整後

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+  
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+  
   def new
     @user = User.new
   end
