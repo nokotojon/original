e2c0e42cbebcf59282ef5103d380f3663984458b
”名前” <>
2020-01-23 15:52:02 +0000

    アカウント作成機能実装前

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_21_160607) do
+ActiveRecord::Schema.define(version: 2020_01_22_060000) do
 
   create_table "songs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2020_01_21_160607) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "image_name"
+    t.string "image"
   end
 
 end
