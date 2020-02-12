b9bef6b79b9ec90368bb6c635a7baceba343e33a
”名前” <>
2020-02-12 06:56:45 +0000

    表示ボタン作成中

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,10 @@
+# encoding: utf-8
+
 class ImageUploader < CarrierWave::Uploader::Base  
 
  # リサイズしたり画像形式を変更するのに必要
   include CarrierWave::RMagick
+  include CarrierWave::MiniMagick
 
  # 画像の上限を700pxにする
   process :resize_to_limit => [700, 700]
@@ -11,7 +14,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # サムネイルを生成する設定
   version :thumb do
-    process :resize_to_limit => [300, 300]
+    process :resize_to_fill => [500, 500]
   end
 
   # jpg,jpeg,gif,pngしか受け付けない
