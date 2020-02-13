4b87cc5be9d20f9d3a4fef01dcb8b29d23adb9cf
”名前” <>
2020-02-13 06:43:57 +0000

    詳細表示機能実装中

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery.turbolinks
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require bootstrap-sprockets
 //= require_tree .
-//= require jquery
-//= require jquery_ujs
