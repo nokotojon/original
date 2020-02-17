c546eb6885d8a21a7c0b075d0b823525bf490d43
”名前” <>
2020-02-17 23:42:52 +0000

    管理者機能実装完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_10_120602) do
+ActiveRecord::Schema.define(version: 2020_02_17_231722) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2020_02_10_120602) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "profile"
+    t.boolean "admin", default: false
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: "管理者",
+             email: "kousei1013z@gmail.com",
+             password: "shinsei9",
+             password_confirmation: "shinsei9",
+             admin:true)
